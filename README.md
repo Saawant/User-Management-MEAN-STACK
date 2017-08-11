19510040b65ee4006451c87419acecd2f6344b4c
Saawant <sawant.sharma@icloud.com>
2017-08-12 02:35:41 +0530

    Readme commit
    -> Updated readme
    -> Updated comments

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# User Management MEAN STACK
+# User Management based on MEAN STACK
+
+
+-> Download index.js & package.json
+-> Run 'npm install' in the directory to install dependencies
+-> To run the server type :
+    node index.js usersDatabase usersCollection 10000
+  where;  usersDatabase     - name of the db you want to associate in mongodb
+          usersCollection   - name of the collection in mongodb
+          10000             - port
+
+
+
+  List of RESTful APIs :
+
+  1) POST (/signup) :
+      To create the user with the following information:
+      username , password  , email , dob , name
+      supplied in the JSON format.
+
+  2) POST (/login)  :
+      For user login :
+      username & password supplied in JSON format.
+
+  3) GET (/users/:userName) :
+      To get user details. Username must be supplied in the URL.
+
+  4) POST (/updateInfo) :
+      To update user details.
+      Only name,email and dob can be changed as of now.  
+
+
+
+Uses bcrypt for password hashing.
--- index.js
+++ index.js
@@ -9,23 +9,34 @@ var table = process.argv[3]
 app.use(bodyParser.json());
 
 
+
+//Connects mongodb on init, if success serves the APIs else returns.
 MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
+  if(err){
+    console.log('Connection unsuccessful');
+    return;
+  }
   console.log("Connected correctly to server.");
   db.close();
+  //Serve the APIs
   app.listen(process.argv[4], function () {
     console.log('App listening on port ' + process.argv[4] + "!")
   })
 });
 
 
+//For user signup
 app.post('/signup', function (request, response) {
+
+  //Checks mandatory fields if found empty or null returns
   if(!request.body.username  || !request.body.name  || !request.body.password  || !request.body.email  ||  !request.body.dob  || request.body.username == '' || request.body.name == '' || request.body.password == '' || request.body.email == '' || reques.body.dob == ''){
     response.send('All fields are mandatory')
     return;
   }
+  //Generate hash to store
   hash = bcrypt.hashSync(request.body.password, 10);
   MongoClient.connect(url, function(err, db) {
+    //Insert user into the db
     db.collection(table).insertOne({
       'username' : request.body.username,
       'name'     : request.body.name,
@@ -33,20 +44,27 @@ app.post('/signup', function (request, response) {
       'email'    : request.body.email,
       'dob'      : reques.body.dob
     }, function(err, result) {
-      assert.equal(err, null);
-      response.send('Signup successful')
+      if(err){
+        assert.equal(err, null);
+      }
+      else {
+        response.send('Signup successful')
+      }
       db.close()
     });
   })
 })
 
+
+//For login
 app.post('/login', function (request, response) {
-  console.log(request.body);
+  //Checks mandatory fields if found empty or null returns
   if(!request.body.username  || !request.body.password  || request.body.username == '' || request.body.password == ''){
     response.send('All fields are mandatory')
     return;
   }
   MongoClient.connect(url, function(err, db) {
+    //Find the user details
     db.collection(table).findOne({ "username": request.body.username },function(err, user) {
       assert.equal(err, null);
       console.log(user);
@@ -68,14 +86,16 @@ app.post('/login', function (request, response) {
 
 
 
-
+//To get specific user details
 app.get('/users/:userName',function(request,response){
   MongoClient.connect(url, function(err, db) {
     db.collection(table).findOne({ "username": request.params.userName },function(err, user) {
       if(user == null){
+        //If no user is found
         response.send('User not found.')
       }
       else {
+        //Return the user data
         response.send(JSON.stringify({
           'username': user.username,
           'email'   : user.email,
@@ -87,7 +107,7 @@ app.get('/users/:userName',function(request,response){
   })
 })
 
-
+//To update user info
 app.post('/updateInfo', function (request, response) {
   MongoClient.connect(url, function(err, db) {
     db.collection(table).updateOne(
